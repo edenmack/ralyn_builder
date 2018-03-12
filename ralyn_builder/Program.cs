61f985cb80e6e6ecbff1e2e42df92d28f36e4a55
eden <eden@variableman.com>
2018-03-12 15:17:58 -0400

    EOD Check in
    lexer improvements

--- ralyn_builder/Program.cs
+++ ralyn_builder/Program.cs
@@ -19,6 +19,8 @@ namespace ralyn_builder
             Operator,
             Comment,
             Literal,
+            Literal_String,
+            Literal_Num,
             TODO,
             WhiteSpace,
             NewLine
@@ -66,30 +68,30 @@ namespace ralyn_builder
             var currentList = noneList;
             var currentTokenType = TokenType.None;
 
-            //set token type
-            if (charListContains(Token.separatorList, c))
+            //set token type            
+            if (charListContains(Token.numStartList, c))
             {
-                currentTokenType = Token.TokenType.Separator;
+                currentTokenType = TokenType.Literal_Num;
+                currentList = Token.numValidList;
+            }
+            else if (charListContains(Token.separatorList, c))
+            {
+                currentTokenType = TokenType.Separator;
                 currentList = Token.separatorList;
             }
             else if (charListContains(Token.operatorList, c))
             {
-                currentTokenType = Token.TokenType.Operator;
+                currentTokenType = TokenType.Operator;
                 currentList = Token.operatorList;
             }
             else if (charListContains(Token.whiteSpaceList, c))
             {
-                currentTokenType = Token.TokenType.WhiteSpace;
+                currentTokenType = TokenType.WhiteSpace;
                 currentList = Token.whiteSpaceList;
-            }
-            else if (charListContains(Token.numStartList, c))
-            {
-                currentTokenType = Token.TokenType.Literal;
-                currentList = Token.numValidList;
             }
             else if (charListContains(Token.identStartList, c))
             {
-                currentTokenType = Token.TokenType.Word;
+                currentTokenType = TokenType.Word;
                 currentList = Token.indentValidList;
             }
 
@@ -103,29 +105,44 @@ namespace ralyn_builder
 
         public TokenType Type;
         public string Value;
+        public int Line;
+        public int Char;
 
         public Token()
         {
             Type = TokenType.None;
             Value = null;
+            Line = -1;
+            Char = -1;
         }
         public Token(TokenType theType, string theValue)
         {
             Type = theType;
             Value = theValue;
+            Line = -1;
+            Char = -1;
+        }
+        public Token(TokenType theType, string theValue, int theLine, int theChar)
+        {
+            Type = theType;
+            Value = theValue;
+            Line = theLine;
+            Char = theChar;
         }
         public Token(string theValue)
         {
             Type = TokenType.Word; //maybe try to figure this out based on theValue
             Value = theValue;
+            Line = -1;
+            Char = -1;
         }
 
         public override string ToString()
         {
             if (this.Type == TokenType.NewLine)
-                return String.Format("[{0}: [NEWLINE]]", this.Type);
+                return $"[({this.Line},{this.Char}){this.Type}: [NEWLINE]]";
             else
-                return String.Format("[{0}: {1}]", this.Type, this.Value);
+                return $"[({this.Line},{this.Char}){this.Type}: {this.Value}]";
         }
     }
     class Program
@@ -133,28 +150,6 @@ namespace ralyn_builder
         //UI
         static void Main(string[] args)
         {
-            var testComment =
-@"   //this
-//comment2
-
-//block comment{
-    this is a comment 
-    block
-    {this is an inner block}
-}
-//TODO: this is something to do;
-";
-            var testNumbers =
-@"6.022e23 -23 1.3 1ab4";
-            var testString =
-@"
-    var s:string = $(_this is a string_);
-    var s1:string = $'this is a string as well';
-    var s2:string = 'this is also a string';
-    var s3:string = ""this is the final string"";
-}
-";
-
             var exit = false;
 
             while (!exit)
@@ -172,7 +167,8 @@ namespace ralyn_builder
                         Console.WriteLine();
                         try
                         {
-                            var result = Lex(testString);
+                            var codeFromFile = System.IO.File.ReadAllText(@"..\..\TestCode.ralyn");
+                            var result = Lex(codeFromFile);
                             foreach (var t in result)
                                 Console.WriteLine(t.ToString());
                         }catch(Exception lexEx)
@@ -193,132 +189,8 @@ namespace ralyn_builder
         }
 
         //Builder
-        #region Lexical Analysis
+        #region Lexical Analysis        
         static List<Token> Lex(string code)
-        {
-            var rawTokens = syntaxSort(code);
-
-            ///Review raw tokens
-            /// Tokens -> Comment Literals
-            /// Tokens -> hints (ex TODO, DOC, etc.)
-            /// Tokens -> String Literals
-            /// Word -> Literal|Keyword
-            /// remove unnecessary white space
-            /// 
-
-            var tokens = new List<Token>();
-            var accumulator = new System.Text.StringBuilder();
-            var nestLevel = 0;
-            var isBlock = false;
-
-            //first pass: COMMENTS
-            isBlock = false;
-            nestLevel = 0;
-            foreach (var t in rawTokens)
-            {
-                if(t.Value == "//"
-                    || isBlock)
-                {
-                    isBlock = true;
-
-                    if(t.Type == Token.TokenType.NewLine && nestLevel == 0)
-                    {
-                        //flush single line comment
-                        tokens.Add(new Token(Token.TokenType.Comment, accumulator.ToString()));
-                        accumulator.Clear();
-                        isBlock = false;
-                    }
-                    else
-                    {
-                        accumulator.Append(t.Value);
-
-                        if (t.Value == "{")
-                            ++nestLevel;
-                        if(t.Value == "}")
-                        {
-                            --nestLevel;
-                            if(nestLevel == 0)
-                            {
-                                //flush multiline comment
-                                tokens.Add(new Token(Token.TokenType.Comment, accumulator.ToString()));
-                                accumulator.Clear();
-                                isBlock = false;
-                            }
-                        }
-                    }
-                }
-                else
-                {
-                    //this is not a comment... just flush the token as is
-                    tokens.Add(t);
-                }
-            }
-
-            //second pass: QUOTES
-            //TODO:  this doesn't work yet... need to look at finding beginning and ends of strings
-            rawTokens.Clear();
-            rawTokens.AddRange(tokens);
-            tokens.Clear();
-            isBlock = false;
-            nestLevel = 0;
-            accumulator.Clear();
-            var endDelimiter = "";
-            foreach(var t in rawTokens)
-            {
-                ///a string begins with $", $', or $(x (where x can be any character) and ends with a matching ", ', or x)
-                /// the $ can be ommitted if you are not using parenthetical notation.
-
-                if(t.Value[0] == '$'
-                    || t.Value[0] == '"'
-                    || t.Value[0] == '\''
-                    || isBlock)
-                {
-                    //this is a string
-                    if (!isBlock)
-                    {
-                        //just discovered the string
-                        isBlock = true;
-                        accumulator.Append(t.Value);
-                    }else
-                    {
-                        //inside string until we discover exit condition 
-                        //TODO: FIX EXIT CONDITIONS!  STILL NOT RIGHT
-                        if (accumulator.Length >= 3)
-                        {
-                            endDelimiter = accumulator.ToString().Substring(0, 3);
-                            endDelimiter.TrimStart(new char[] { '$' });
-                            if (endDelimiter[0] == '(')
-                                endDelimiter = endDelimiter[1] + ")";
-                            else
-                                endDelimiter = endDelimiter[0].ToString();
-                        }
-                        
-                        if (!String.IsNullOrEmpty(endDelimiter) && t.Value.IndexOf(endDelimiter) != -1)
-                        {
-                            accumulator.Append(t.Value.Substring(t.Value.IndexOf(endDelimiter)));
-                            tokens.Add(new Token(Token.TokenType.Literal, accumulator.ToString()));
-                            accumulator.Clear();
-                            accumulator.Append(t.Value.Substring(t.Value.IndexOf(endDelimiter)));
-                            isBlock = false;
-                            endDelimiter = "";
-                        }
-                        else
-                        {
-                            accumulator.Append(t.Value);
-                        }
-                        
-                    }
-                }
-                else
-                {
-                    //not a string
-                    tokens.Add(t);
-                }
-            }
-
-            return tokens;
-        }
-        static List<Token> syntaxSort(string code)
         {
             //setup vars
             var tokens = new List<Token>();
@@ -327,24 +199,107 @@ namespace ralyn_builder
             {
                 var lineNum = 0;
                 var charNum = 0;
+                var tokenLineStart = 0;
+                var tokenCharStart = 0;
+                Token.TokenWatcher currentStatus = new Token.TokenWatcher();
+                currentStatus.Type = Token.TokenType.None;
+                currentStatus.CharacterList = new List<char>() { };
+                var tokenAccumulator = new StringBuilder();
+                var nestLevel = 0;
+                var stringEndSequence = "";
 
                 while (_code.Peek() > -1)
                 {
                     line = _code.ReadLine().Trim();
+                    ++lineNum;
+                    if (currentStatus.Type == Token.TokenType.None)
+                        tokenLineStart = lineNum;
                     charNum = 0;
-                    Token.TokenWatcher currentStatus = new Token.TokenWatcher();
-                    currentStatus.Type = Token.TokenType.None;
-                    currentStatus.CharacterList = new List<char>() { };
-                    var tokenAccumulator = new StringBuilder();
+                    
 
                     foreach (char c in line)
                     {
+                        ++charNum;
                         if (currentStatus.Type == Token.TokenType.None)
+                            tokenCharStart = charNum;
+
+                        if ((tokenAccumulator.Length >= 1 && (tokenAccumulator[0] == '\'' || tokenAccumulator[0] == '"'))
+                            || (tokenAccumulator.Length > 1 && tokenAccumulator[0] == '$' && tokenAccumulator[1] == '('))
+                        {
+                            //this is a string
+                            currentStatus.Type = Token.TokenType.Literal_String;
+
+                            tokenAccumulator.Append(c);
+
+                            if (tokenAccumulator[0] == '"') stringEndSequence = "\"";
+                            if (tokenAccumulator[0] == '\'') stringEndSequence = "'";
+                            if (tokenAccumulator.Length > 2 && tokenAccumulator[0] == '$' && tokenAccumulator[1] == '(')
+                            {
+                                stringEndSequence = tokenAccumulator[2] + ")";
+                            }
+
+                            if(tokenAccumulator.Length>=stringEndSequence.Length*2 
+                                && tokenAccumulator.ToString().Substring(tokenAccumulator.Length - stringEndSequence.Length) == stringEndSequence)
+                            {
+                                //found end of string -- flush
+                                tokens.Add(new Token(currentStatus.Type, tokenAccumulator.ToString(),tokenLineStart,tokenCharStart));
+                                tokenAccumulator.Clear();
+                                currentStatus.Type = Token.TokenType.None;
+                                continue;
+                            }
+                            if (charNum == line.Length) tokenAccumulator.Append(Environment.NewLine);
+
+                            continue;                            
+                        }else if (tokenAccumulator.Length > 1 && tokenAccumulator[0] == '/' && tokenAccumulator[1] == '/')
+                        {
+                            //this is a comment
+                            currentStatus.Type = Token.TokenType.Comment;
+
+                            tokenAccumulator.Append(c);
+
+                            if (c == '{')
+                                ++nestLevel;
+                            if (c == '}')
+                            {
+                                --nestLevel;
+                                if (nestLevel == 0)
+                                {
+                                    //flush block comment
+                                    if (tokenAccumulator.Length > 0)
+                                    {
+                                        tokens.Add(new Token(currentStatus.Type, tokenAccumulator.ToString(),tokenLineStart,tokenCharStart));
+                                        tokenAccumulator.Clear();
+                                        currentStatus.Type = Token.TokenType.None;
+                                    }
+                                    continue;
+                                }
+                            }
+
+                            if(charNum == line.Length)
+                            {                                
+                                if(nestLevel == 0)
+                                {
+                                    //flush single line comment
+                                    if (tokenAccumulator.Length > 0)
+                                    {
+                                        tokens.Add(new Token(currentStatus.Type, tokenAccumulator.ToString(), tokenLineStart, tokenCharStart));
+                                        tokenAccumulator.Clear();
+                                        currentStatus.Type = Token.TokenType.None;
+                                    }
+                                    continue;
+                                }
+                                else
+                                {
+                                    tokenAccumulator.Append(Environment.NewLine);
+                                }
+                                
+                            }
+                        }else if (currentStatus.Type == Token.TokenType.None)
                         {
                             //set token type
                             currentStatus = Token.getTokenCharType(c);
                             if (currentStatus.CharacterList.Count == 0)
-                                throw new Exception($"Unexpected character '{c}' at line {lineNum} character {charNum}{System.Environment.NewLine}---->{line.Substring(0, charNum)} >>{c}<< {line.Substring(charNum + 1)}");
+                                throw new Exception($"Unexpected character '{c}' at line {lineNum} character {charNum}{Environment.NewLine}---->{line.Substring(0, charNum-1)} >>{c}<<");
 
                             //append first character
                             tokenAccumulator.Append(c);
@@ -359,33 +314,19 @@ namespace ralyn_builder
                             else
                             {
                                 //flush
-                                tokens.Add(new Token(currentStatus.Type, tokenAccumulator.ToString()));
+                                tokens.Add(new Token(currentStatus.Type, tokenAccumulator.ToString(), tokenLineStart, tokenCharStart));
                                 tokenAccumulator.Clear();
 
                                 //get new token type
                                 currentStatus = Token.getTokenCharType(c);
                                 if (currentStatus.CharacterList.Count == 0)
-                                    throw new Exception($"Unexpected character '{c}' at line {lineNum} character {charNum}{System.Environment.NewLine}---->{line.Substring(0, charNum)} >>{c}<< {line.Substring(charNum+1)}");
+                                    throw new Exception($"Unexpected character '{c}' at line {lineNum} character {charNum}{Environment.NewLine}---->{line.Substring(0, charNum-1)} >>{c}<<");
 
                                 //append first character
                                 tokenAccumulator.Append(c);
                             }
                         }
-                        ++charNum;
-                    }
-
-                    //flush end of line
-                    if (tokenAccumulator.Length > 0)
-                    {
-                        tokens.Add(new Token(currentStatus.Type, tokenAccumulator.ToString()));
-                    }
-                    tokens.Add(new Token(Token.TokenType.NewLine, System.Environment.NewLine));
-
-                    //reset for next line
-                    tokenAccumulator.Clear();
-                    currentStatus.Type = Token.TokenType.None;
-
-                    ++lineNum;
+                    }                    
                 }
             }
 
